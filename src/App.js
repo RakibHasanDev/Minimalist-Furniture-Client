b8caaf5d2777274f0b0ca945d6b14e72b458d87c
MrMernDeveloper <rakibkst16@gmail.com>
2022-11-24 03:01:40 +0600

    added authprovider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-    <h1 className='text-3xl'>This is home</h1>
+    <div className="w-11/12 mx-auto ">
+    <h1 className='text-3xl text-primary text-center'>This is home</h1>
     </div>
   );
 }
