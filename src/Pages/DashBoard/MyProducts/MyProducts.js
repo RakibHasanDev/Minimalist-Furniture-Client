ca214e3b3be6f6f474255932d1eebe4d8e11dc2a
MrMernDeveloper <rakibkst16@gmail.com>
2022-11-26 20:37:34 +0600

    added my products

--- src/Pages/DashBoard/MyProducts/MyProducts.js
+++ src/Pages/DashBoard/MyProducts/MyProducts.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useQuery } from 'react-query';
+import Loading from '../../../Components/Loading';
+import { AuthContext } from '../../../Context/AuthProvider';
 
 const MyProducts = () => {
+    const { user } = useContext(AuthContext);
+    const url = `http://localhost:5000/products?email=${user?.email}`
+
+    const { data: myProducts = [], isLoading } = useQuery({
+        queryKey: ['products', user?.email],
+        queryFn: async () => {
+            const res = await fetch(url);
+            const data = await res.json();
+            return data;
+        }
+
+    })
+    if (isLoading) {
+        return <Loading></Loading>
+    }
     return (
         <div>
-            <h1> My Products</h1>
+            <h1> My Products { myProducts.length}</h1>
         </div>
     );
 };
